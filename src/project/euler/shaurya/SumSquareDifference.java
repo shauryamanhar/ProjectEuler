13084162ca4d0480ed528593369ca08e709476aa
shaur <shaur@UNDERFLOW-PC>
2020-05-05 12:33:12 +0530

    Sum square difference

---
+++ src/project/euler/shaurya/SumSquareDifference.java
@@ -0,0 +1,11 @@
+package project.euler.shaurya;
+
+public class SumSquareDifference {
+	public static void main(String[] args) {
+		long n=100;
+		long sumN2 = ((n)*(n+1)*(2*n+1))/6;
+		long sumN = (n*(n+1))/2;
+		sumN = sumN*sumN;
+		System.out.println(sumN-sumN2);
+	}
+}
